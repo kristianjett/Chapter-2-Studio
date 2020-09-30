e17ea79e09e67386be505954d3357c024acb603a
Kristian <jettkristian@gmail.com>
2020-09-29 19:03:24 -0700

    Add project files.

---
+++ Chapter 2 Studio/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Circle;
+
+namespace Chapter_2_Studio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a radius: ");
+
+            double radius = double.Parse(Console.ReadLine());
+
+            double pi = Math.PI;
+
+            double areaOfCircle = Math.Round((pi * Math.Pow(radius, 2)), 3);
+
+            double circumferenceOfCircle = Math.Round((2 * pi * radius), 3);
+
+            double diameterOfCircle = radius * 2;
+
+            Console.WriteLine($"The area of a circle with radius {radius} is: {areaOfCircle}; \n" +
+                $"The circumference is: {circumferenceOfCircle}; \n" +
+                $"The diameter is: {diameterOfCircle}.");
+
+            Console.WriteLine("How many miles per gallon do you get?");
+
+            double milesPerGallon = 1 / double.Parse(Console.ReadLine());
+
+            double gallonsUsed = Math.Round((milesPerGallon * radius), 2);
+
+            Console.WriteLine($"It would take {gallonsUsed} gallons of gas to go around this circle.");
+
+        }
+    }
+}
---
+++ Circle/Circle.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Circle
+{
+    public class Circle
+    {
+        private double radius;
+        private double pi = Math.PI;
+        private double areaOfCircle = Math.Round((this.pi * Math.Pow(this.radius, 2)), 3);
+    }
+}
